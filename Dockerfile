e813c7bc2d737b6e9b954d33baf3a8d9c6d18ace
EdRamos12 <eduardo_ramos12@protonmail.ch>
2024-03-14 15:18:21 -0300

    init commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# syntax=docker/dockerfile:1
+# Dockerfile for combining multiple services
+FROM ubuntu:latest
+RUN apt-get update && apt-get install -y supervisor wget
+
+# Elasticsearch Exporter
+RUN wget https://github.com/prometheus-community/elasticsearch_exporter/releases/download/v1.7.0/elasticsearch_exporter-1.7.0.linux-amd64.tar.gz
+RUN tar -xzf elasticsearch_exporter-1.7.0.linux-amd64.tar.gz
+RUN mv elasticsearch_exporter-*/elasticsearch_exporter /bin
+
+RUN wget https://github.com/prometheus-community/PushProx/releases/download/v0.1.0/PushProx-0.1.0.linux-amd64.tar.gz
+RUN tar -xzf PushProx-0.1.0.linux-amd64.tar.gz
+RUN mv PushProx-0.1.0.linux-amd64/pushprox-client /bin
+
+# Copy supervisord configuration file
+COPY supervisord.conf /etc/supervisor/conf.d/
+
+COPY run_exporter.sh /bin
+
+RUN chmod +x /bin/run_exporter.sh
+
+EXPOSE 8080/tcp
+
+# Start supervisord
+CMD ["/bin/run_exporter.sh"]
---
+++ run_exporter.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check if ELASTIC_URL environment variable is set
+if [ -z "$ELASTIC_URL" ]; then
+    echo "ELASTIC_URL environment variable is not set."
+    exit 1
+fi
+
+# Remove http:// or https:// from ELASTIC_URL if present
+ELASTIC_URL_CLEAN=$(echo "$ELASTIC_URL" | sed -e 's/^http[s]*:\/\///')
+
+# Store protocol in PROTOCOL variable
+if [[ "$ELASTIC_URL" == https://* ]]; then
+    PROTOCOL="https"
+elif [[ "$ELASTIC_URL" == http://* ]]; then
+    PROTOCOL="http"
+else
+    echo "Protocol not found in ELASTIC_URL. Using unsafe protocol."
+    PROTOCOL="http"
+fi
+
+echo "Cleaned ELASTIC_URL: $ELASTIC_URL_CLEAN"
+echo "Protocol: $PROTOCOL"
+
+# Set ELASTIC_URL to cleaned value
+export ELASTIC_URL="$ELASTIC_URL_CLEAN"
+export PROTOCOL="$ELASTIC_URL_CLEAN"
+
+echo "ELASTIC_URL updated."
+
+echo "Initializing exporter..."
+
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
