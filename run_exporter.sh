78478545062ed0b953106384ccf0d7b8915c7b37
EdRamos12 <eduardo_ramos12@protonmail.ch>
2024-07-23 22:39:52 -0300

    fix: detect new protocol from proxy ip

--- run_exporter.sh
+++ run_exporter.sh
@@ -8,6 +8,11 @@ if [ -z "$ELASTIC_URL" ]; then
     exit 1
 fi
 
+if [ -z "$PROXY_IP" ]; then
+    echo "PROXY_IP environment variable is not set."
+    exit 1
+fi
+
 if [ -z "$PROTOCOL" ]; then
     # Store protocol in PROTOCOL variable
     if [[ "$ELASTIC_URL" == https://* ]]; then
@@ -20,21 +25,38 @@ if [ -z "$PROTOCOL" ]; then
     fi
 fi
 
+if [[ "$PROXY_IP" == https://* ]]; then
+    PROXY_PROTOCOL="https"
+else
+    PROXY_PROTOCOL="http"
+fi
+
 # Remove http:// or https:// from ELASTIC_URL if present
 ELASTIC_URL_CLEAN=$(echo "$ELASTIC_URL" | sed -e 's/^http[s]*:\/\///')
+
+# Remove http:// or https:// from PROXY_IP if present
+PROXY_IP_CLEAN=$(echo "$PROXY_IP" | sed -e 's/^http[s]*:\/\///')
 
 # Set ELASTIC_URL to cleaned value
 export ELASTIC_URL="$ELASTIC_URL_CLEAN"
 export PROTOCOL
+
+export PROXY_IP="$PROXY_IP_CLEAN"
+export PROXY_PROTOCOL
 
 if [ -z "$ENVIROMENT_ALREADY_SETUP" ] || [ "$ENVIROMENT_ALREADY_SETUP" != "true" ]; then
     echo "Initializing monitoring instance..."
 
     MACHINE_NAME=$(hostname)
 
-    response=$(curl -F instance_id=$MACHINE_NAME -X POST http://$PROXY_IP:443/monitoring/create_instance/$TOKEN)
+    response=$(curl -F instance_id=$MACHINE_NAME -X POST $PROXY_PROTOCOL://$PROXY_IP:443/monitoring/create_instance/$TOKEN)
 
     port=$(echo "$response" | jq -r '.port')
+
+    if [ "$port" == "null" ]; then
+        echo "Error while retrieving port from the proxy. (Are the enviroment variables correct?)"
+        exit 1
+    fi
 
     echo "#"
     echo "#"
@@ -42,7 +64,7 @@ if [ -z "$ENVIROMENT_ALREADY_SETUP" ] || [ "$ENVIROMENT_ALREADY_SETUP" != "true"
     echo "#"
     echo "#"
     echo "# Your Grafana instance is now up!"
-    echo "# You can access it from your browser at http://$PROXY_IP:$port/"
+    echo "# You can access it from your browser at $PROXY_PROTOCOL://$PROXY_IP:$port/"
     echo "#"
     echo "#"
     echo "#"
